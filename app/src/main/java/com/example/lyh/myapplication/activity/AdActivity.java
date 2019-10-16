b517a41fd4e9de90cfaad149b7338bf039d5377e
huatec-1 <2546763858@qq.com>
2019-10-16 11:34:34 +0800

    第三次上传

--- app/src/main/java/com/example/lyh/myapplication/activity/AdActivity.java
+++ app/src/main/java/com/example/lyh/myapplication/activity/AdActivity.java
@@ -2,6 +2,7 @@ package com.example.lyh.myapplication.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,18 +15,39 @@ import java.time.Instant;
 
 public class AdActivity extends AppCompatActivity {
     TextView tv_ad;
+    Handler handler=new Handler();
+    boolean isStope;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ad);
 
        tv_ad = findViewById(R.id.tv_ad);
+       tv_ad.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               isStope=true;
+               Intent intent3 = new Intent(AdActivity.this, MainActivity.class);
+               startActivity(intent3);
+               finish();
+           }
+       });
+           isStope=false;
 
-        new Thread(){
+        new Thread(new Runnable() {
             @Override
             public void run() {
+
+            }
+        }){
+            @Override
+            public void run() {
+                /*int i;*/
                 super.run();
-                for (int i=5;i>=0;i--){
+                for (int i=5;i>=0;i--) {
+                    if (isStope){
+                        return;
+                    }
                     SystemClock.sleep(1000);
                     final int count=i;
                     runOnUiThread(new Runnable() {
--- app/src/main/java/com/example/lyh/myapplication/fragment/HomeFragment.java
+++ app/src/main/java/com/example/lyh/myapplication/fragment/HomeFragment.java
@@ -1,20 +1,52 @@
 package com.example.lyh.myapplication.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.lyh.myapplication.R;
 
 public class HomeFragment extends Fragment {
+    private WebView  WebView ;
+    private SwipeRefreshLayout refresh;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        WebView  = view.findViewById(R.id.webview);
+        refresh=view.findViewById(R.id.refresh);
+        initView();
         return view;
+    }
+
+    private void initView(){
+        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh.setRefreshing(false);
+                WebView.loadUrl("https://www.huawei.com/cn/");
+            }
+        });
+        WebView.getSettings().setSupportZoom(true);
+        WebView.getSettings().setJavaScriptEnabled(true);
+     WebView.setWebViewClient(new WebViewClient(){
+         @Override
+         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+             view.loadUrl(url);
+             return true;
+         }
+
+
+     });
+       WebView.loadUrl("https://www.huawei.com/cn/");
     }
 }
